1045a0e69064f200db1def8c2c247be6617471ae
okelbok <vladakolbeko@yandex.by>
2025-03-12 21:10:14 +0300

    init javafx + maven

---
+++ src/main/module-info.java
@@ -0,0 +1,15 @@
+module by.custom_paint {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens by.custom_paint to javafx.fxml;
+    exports by.custom_paint;
+}
